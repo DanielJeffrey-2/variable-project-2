a50844ca59ba4539946e71a4ee11b4b8849a2bd3
DanielJeffrey-2 <113033888+DanielJeffrey-2@users.noreply.github.com>
2022-09-27 09:14:24 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    counter += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(counter)
+})
+input.onButtonPressed(Button.B, function () {
+    counter += -1
+})
+/**
+ * Created by: Daniel Jeffrey
+ * 
+ * Created on: Sept 27, 2022
+ * 
+ * This program is used to demonstrate a simple variable and counter for button presses.
+ */
+let counter = 0
+counter = 0
